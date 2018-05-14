a9e485496c232c61a3435f9d8c55c2385f6b70c7
Matthew B White <whitemat@uk.ibm.com>
2018-05-14 12:14:12 +0100

    npm production does not work
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- .localtoolchain/npminstall-all.sh
+++ .localtoolchain/npminstall-all.sh
@@ -4,19 +4,19 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
 echo Commercial Paper Network
 cd "${DIR}"/contracts/commercial-paper-network
-npm install  --production 
+npm install  
 
 echo Command line apps
 cd "${DIR}"/apps/cp-cli
-npm install  --production 
+npm install   
 
 echo Commercial Paper Web UI
 cd "${DIR}"/apps/aai-web
-npm install  --production 
+npm install   
 
 echo DID-manger
 cd "${DIR}"/apps/did-manager
-npm install  --production 
+npm install   
 
 # echo Angular2 generated app 
 # cd "${DIR}"/apps/ami-web/ami-web
