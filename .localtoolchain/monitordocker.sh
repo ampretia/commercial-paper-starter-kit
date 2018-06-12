5182d29129d2a380d8947cf715eb3a72b46fa428
Matthew B White <whitemat@uk.ibm.com>
2018-06-12 10:38:59 +0100

    update to fix the ip address of the monitor
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>

--- .localtoolchain/monitordocker.sh
+++ .localtoolchain/monitordocker.sh
@@ -4,6 +4,7 @@ docker start logspout || docker run -d --name="logspout" \
 	--volume=/var/run/docker.sock:/var/run/docker.sock \
 	--publish=127.0.0.1:8000:80 \
 	--network=cp-stack-network \
+	--ip 172.25.0.42   \
 	gliderlabs/logspout  
 
 curl http://127.0.0.1:8000/logs
